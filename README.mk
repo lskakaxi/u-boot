5b6377dc8b5343587969df7151ef93f39b55aaf8
Liu Shuo <shuox.liu@gmail.com>
2014-09-10 19:45:00 +0800

    add README.mk for github
    Signed-off-by: Liu Shuo <shuox.liu@gmail.com>

---
+++ README.mk
@@ -0,0 +1,16 @@
+u-boot
+======
+```
+tq2440_port branch is for TQ2440
+
+Compile:
+
+    - git checkout tq2440_port
+    - make tq2440_config
+    - CROSS_COMPILE=~/bin/gcc-arm-none-eabi-4_8-2014q2/bin/arm-none-eabi- make
+
+Debug:
+
+    - refer to debug directory
+
+```
